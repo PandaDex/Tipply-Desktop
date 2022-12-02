8f9f93f125aed5ada7ab528d5ae943da4282dc6f
Kacper Wochniak <78349074+PandaDex@users.noreply.github.com>
2022-12-02 03:43:50 +0100

    1.0.1

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+const electron = require('electron');
+const nativeTheme = electron.nativeTheme;
+
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 1600,
+    height: 900,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+  mainWindow.loadURL('https://tipply.pl');
+  mainWindow.setMenu(null);
+  mainWindow.setIcon(path.join(__dirname, 'icon.png'));
+  nativeTheme.themeSource = 'system';
+  mainWindow.setBackgroundColor('#000')
+
+};
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
