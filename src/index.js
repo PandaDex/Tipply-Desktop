c978194438c82f6bce097c599def08c0ff0cc15a
Kacper Wochniak <78349074+PandaDex@users.noreply.github.com>
2023-01-19 14:46:24 +0100

    1.0.4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Notification } = require('electron');
+const superagent = require('superagent');
+app.setAppUserModelId("Tipply Desktop")
+
+
 const path = require('path');
 if (require('electron-squirrel-startup')) {
   app.quit();
@@ -15,6 +19,28 @@ const createWindow = () => {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
+
+superagent.get('https://api.github.com/repos/PandaDex/Tipply-Desktop/releases/latest').set('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13').set('Content-Type', 'application/json').end((err, res) => {
+  const latest = res.body.name;
+  const local = require('../package.json').version;
+  
+ if(latest > local) {
+  const updateico = path.join(__dirname, 'update.ico')
+  let iconAddress = path.join(__dirname, 'icon.png');
+  const notif={
+        title: 'Tipply',
+        body: `Dostepna jest nowa aktualizacja: ${latest}`,
+        icon: iconAddress
+      };
+  noti = new Notification(notif)
+  noti.show()
+  noti.on('click', ()=>{
+    electron.shell.openExternal("https://github.com/PandaDex/Tipply-Desktop/releases/tag/Latest");
+  })
+  mainWindow.setOverlayIcon(updateico, 'Dostepna jest aktualizacja!')
+ }
+})
+
   mainWindow.loadURL('https://app.tipply.pl');
   mainWindow.setMenu(null);
   mainWindow.setIcon(path.join(__dirname, 'icon.png'));
